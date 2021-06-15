a3d16593ff0f31948756bea02cf7f9244ed896de
Charly_Angel <c.a.a.a.503@gmail.com>
2021-06-15 18:21:20 -0500

    refactor(category): refactorizando los métodos create, update y delete de categories.service

--- src/products/services/categories.service.ts
+++ src/products/services/categories.service.ts
@@ -15,40 +15,26 @@ export class CategoriesService {
     return this.categoryRepo.find();
   }
 
-  findOne(id: number) {
-    const category = this.categoryRepo.findOne(id);
+  async findOne(id: number) {
+    const category = await this.categoryRepo.findOne(id);
     if (!category) {
       throw new NotFoundException(`Category #${id} not found`);
     }
     return category;
   }
 
-  // create(data: CreateCategoryDto) {
-  //   this.counterId = this.counterId + 1;
-  //   const newCategory = {
-  //     id: this.counterId,
-  //     ...data,
-  //   };
-  //   this.categories.push(newCategory);
-  //   return newCategory;
-  // }
+  create(data: CreateCategoryDto) {
+    const newCategory = this.categoryRepo.create(data);
+    return this.categoryRepo.save(newCategory);
+  }
 
-  // update(id: number, changes: UpdateCategoryDto) {
-  //   const category = this.findOne(id);
-  //   const index = this.categories.findIndex((item) => item.id === id);
-  //   this.categories[index] = {
-  //     ...category,
-  //     ...changes,
-  //   };
-  //   return this.categories[index];
-  // }
+  async update(id: number, changes: UpdateCategoryDto) {
+    const category = await this.categoryRepo.findOne(id);
+    this.categoryRepo.merge(category, changes);
+    return this.categoryRepo.save(category);
+  }
 
-  // remove(id: number) {
-  //   const index = this.categories.findIndex((item) => item.id === id);
-  //   if (index === -1) {
-  //     throw new NotFoundException(`Category #${id} not found`);
-  //   }
-  //   this.categories.splice(index, 1);
-  //   return true;
-  // }
+  remove(id: number) {
+    return this.categoryRepo.delete(id);
+  }
 }
