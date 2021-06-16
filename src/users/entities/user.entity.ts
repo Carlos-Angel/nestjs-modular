6041ff2f2de618ad256e86edd7eeb022e1e2759d
Charly_Angel <c.a.a.a.503@gmail.com>
2021-06-16 14:18:26 -0500

    refactor(database): definición de entidades user y costumer

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,42 @@
+import {
+  PrimaryGeneratedColumn,
+  Column,
+  Entity,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import { Customer } from './customer.entity';
+
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @OneToOne(() => Customer, (customer) => customer.user, { nullable: true })
+  @JoinColumn()
+  customer: Customer;
+
+  @Column({ type: 'varchar', length: 255, unique: true })
   email: string;
-  password: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  password: string; // encript
+
+  @Column({ type: 'varchar', length: 255 })
   role: string;
+
+  @CreateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAtt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updatedAtt: Date;
 }
