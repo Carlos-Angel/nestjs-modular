012d1103b20ae2fe27813017fdffb030b80540ce
Charly_Angel <c.a.a.a.503@gmail.com>
2021-06-28 15:20:31 -0500

    feat: autenticación con passport

---
+++ src/auth/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from '../../users/services/users.service';
+
+import { isMatchPasswords } from '../../utils/handler-bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(private userService: UsersService) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+    if (!user) return null;
+
+    const isMatch = await isMatchPasswords(user.password, password);
+
+    if (isMatch) return user;
+    else return null;
+  }
+}
--- src/utils/handler-bcrypt.ts
+++ src/utils/handler-bcrypt.ts
@@ -3,3 +3,7 @@ import * as bcrypt from 'bcrypt';
 export const hashPassword = async (password) => {
   return await bcrypt.hash(password, 10);
 };
+
+export const isMatchPasswords = async (hash, password) => {
+  return await bcrypt.compare(hash, password);
+};
