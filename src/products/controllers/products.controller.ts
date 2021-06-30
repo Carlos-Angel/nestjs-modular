c9b4fd3e5d08f6d6bb7afd0f7e3bd30fcbcfad5b
Charly_Angel <c.a.a.a.503@gmail.com>
2021-06-29 20:10:56 -0500

    feat: manejo de roles en endpoints de products

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -22,9 +22,12 @@ import {
 } from '../dtos/products.dto';
 import { ProductsService } from './../services/products.service';
 import { JwtAuthGuard } from './../../auth/guards/jwt-auth.guard';
+import { RolesGuard } from './../../auth/guards/roles.guard';
 import { Public } from '../../auth/decorators/public.decorator';
+import { Roles } from '../../auth/decorators/roles.decorator';
+import { Role } from '../../auth/models/role.model';
 
-@UseGuards(JwtAuthGuard)
+@UseGuards(JwtAuthGuard, RolesGuard)
 @ApiTags('products')
 @Controller('products')
 export class ProductsController {
@@ -52,21 +55,25 @@ export class ProductsController {
     return this.productsService.findOne(productId);
   }
 
+  @Roles(Role.ADMIN)
   @Post()
   create(@Body() payload: CreateProductDto) {
     return this.productsService.create(payload);
   }
 
+  @Roles(Role.ADMIN)
   @Put(':id')
   update(@Param('id') id: number, @Body() payload: UpdateProductDto) {
     return this.productsService.update(id, payload);
   }
 
+  @Roles(Role.ADMIN)
   @Delete(':id')
   delete(@Param('id') id: number) {
     return this.productsService.remove(id);
   }
 
+  @Roles(Role.ADMIN)
   @Put(':id/category/:categoryId')
   addCategory(
     @Param('id', ParseIntPipe) id: number,
@@ -75,6 +82,7 @@ export class ProductsController {
     return this.productsService.addCategoryToProduct(id, categoryId);
   }
 
+  @Roles(Role.ADMIN)
   @Delete(':id/category/:categoryId')
   deleteCategory(
     @Param('id', ParseIntPipe) id: number,
